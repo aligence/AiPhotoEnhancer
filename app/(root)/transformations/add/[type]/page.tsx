b8c8ae769f4cd9a9c59455541e0b6bbb60cf5abd
Alireza Manizani <persianboy347@gmail.com>
2024-02-24 19:20:57 -0800

    image details

--- app/(root)/transformations/add/[type]/page.tsx
+++ app/(root)/transformations/add/[type]/page.tsx
@@ -1,8 +1,23 @@
-import React from 'react'
+import Header from '@/components/shared/Header'
+import { transformationTypes } from '@/constants'
+import {auth} from '@clerk/nextjs'
+import TransformationForm from '@/components/shared/TransformationForm';
+import { getUserById } from '@/lib/actions/user.actions';
+import { redirect } from 'next/navigation';
 
-const AddTransformationTypePage = () => {
+const AddTransformationTypePage = async ({params : {type} }: SearchParamProps) => {
+  const {userId} = auth()
+  if(!userId) redirect('/sing-in')
+  const user = await getUserById(userId)
+  const transformation = transformationTypes[type];
   return (
-    <div>AddTransformationTypePage</div>
+    <>
+      <Header title={transformation.title} subtitle = {transformation.subTitle}/>
+
+      <TransformationForm
+      action = "Add" userId={user._id} type = {transformation.type as TransformationTypeKey} creditBalance={user.creditBalance}/>
+    </>
+    
   )
 }
 
